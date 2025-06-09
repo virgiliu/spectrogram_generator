40ccda70941c8493d0b7f8029ce78fe4c1cc4c34
Virgiliu <426097+virgiliu@users.noreply.github.com>
2025-06-09 16:33:35 +0200

    Load Celery broker URL from env var if present, otherwise fall back on hardcoded value.

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,8 +1,10 @@
+import os
+
 from celery import Celery
 
-celery_app = Celery(
-    "tasks", broker="redis://localhost:6379/0", backend="redis://localhost:6379/0"
-)
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+
+celery_app = Celery("tasks", broker=CELERY_BROKER_URL, backend=CELERY_BROKER_URL)
 
 celery_app.conf.task_acks_late = True
 celery_app.conf.worker_prefetch_multiplier = 1
